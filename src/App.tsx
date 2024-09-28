99c966a7eb6f44fc6442be8101b0497b8573c71b
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2024-09-28 00:03:19 -0700

    Add initial template for landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import './App.css'
+import LandingPage from "./domain/LandingPage/LandingPage.tsx";
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-500">
-        Hello, Tailwind with React and TypeScript!
-      </h1>
-      <form className="mt-5">
-        <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-        <input type="email" id="email"
-               className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"/>
-      </form>
-    </div>
+    <>
+      <LandingPage />
+    </>
   )
 }
 
