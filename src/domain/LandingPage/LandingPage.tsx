ace23e2257dc2e5fe029f90fa4113b88965264df
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2024-09-29 09:29:21 -0700

    Add final CTA

--- src/domain/LandingPage/LandingPage.tsx
+++ src/domain/LandingPage/LandingPage.tsx
@@ -163,23 +163,19 @@ export default function LandingPage() {
           </div>
           <div className="mx-auto max-w-2xl text-center">
             <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-              Boost your productivity.
+              Transcribe your voice notes
               <br />
               Start using our app today.
             </h2>
             <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
-              Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur
-              commodo do ea.
+              Stop wasting time on manual transcription. Let AudioPal handle your voice-to-text needs.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
                 className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
-                Get started
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-white">
-                Learn more <span aria-hidden="true">→</span>
+                Start Now on Telegram.
               </a>
             </div>
           </div>
